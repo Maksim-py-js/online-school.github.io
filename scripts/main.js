69a6446f206aabf3580b98a77f2d6086f75e98cd
Maksim-py-js <muhamedzyanov2004@gmail.com>
2021-03-23 21:59:28 +0500

    make block with articles

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,8 @@ const data = document.getElementsByClassName("progress-bar");
 
 const data_bar_left = document.getElementsByClassName("progress-bar-left");
 const data_bar_right = document.getElementsByClassName("progress-bar-right");
-const data_bar_number = document.getElementsByClassName("progress-bar-number");
+const data_bar_percent = document.getElementsByClassName("progress-bar-percent");
+const data_bar_count = document.getElementsByClassName("progress-bar-count");
 
 var progressState = true;
 
@@ -13,10 +14,15 @@ function progressBar() {
 		const bar_left = data_bar_left[i];
 		const bar_right = data_bar_right[i];
 
-		const bar_number = data_bar_number[i];
+		const bar_percent = data_bar_percent[i];
+		const bar_count = data_bar_count[i];
 
 		let counter = 0;
 		let progress = progress_bar.dataset.progress;
+		let progressessPlus = progress + 1;
+
+		let progressCount = bar_count.dataset.count;
+		let counterCount = (progressCount / progress);
 
 		let counterLeft = 0;
 		let progressLeft = 1.8 * progress * 2;
@@ -30,7 +36,16 @@ function progressBar() {
 			  clearInterval();
 			} else{
 			  counter+=1;
-			  bar_number.textContent = counter + "%";
+			  bar_percent.textContent = counter + "%";
+			}
+		}, 20);
+
+		setInterval(()=>{
+			if(counter == progress){
+			  clearInterval();
+			} else{
+			  counterCount+=(progressCount / progress);
+			  bar_count.textContent = Math.round(counterCount);
 			}
 		}, 20);
 
@@ -64,11 +79,13 @@ function progressBar() {
 }
 function clearProgressBar() {
 	for (var i = 0; i < data.length; i++) {
-		const bar_number = data_bar_number[i];
+		const bar_percent = data_bar_percent[i];
+		const bar_count = data_bar_count[i];
 		const bar_left = data_bar_left[i];
 		const bar_right = data_bar_right[i];
 
-		bar_number.textContent = "0%";
+		bar_percent.textContent = "0%";
+		bar_count.textContent = "0";
 		bar_left.style.transform = 'rotate(0deg)';
 		bar_right.style.transform = 'rotate(0deg)';		
 	}
@@ -89,3 +106,110 @@ $(window).scroll(function() {
 		progressState = true;
 	}
 });
+
+// gsap.to("#trajectory1", {
+//   scrollTrigger: {
+//       trigger: "#trajectory-container",
+//         scrub: true,
+//         markers: true,
+//         start: "start 80%",
+//         end: "bottom 40%"
+//         onEnter: () => $(this).addClass('active'),
+//         onLeave: () => $(this).removeClass('active')
+//   },
+//   opacity: () => {
+//       return (1);
+//   }
+// });
+
+// $("#trajectory1").each(function() {
+//     // Content Reveal Animation
+//     ScrollTrigger.create({
+//       trigger: "#trajectory-container",	
+// 		markers: true,
+
+//       start: "top 20%",
+//       end: "bottom 10%",
+//       onEnter: () => $(this).addClass('active'),
+//       // onLeave: () => $(this).removeClass('active'),
+//       onEnterBack: () => $(this).addClass('active'),
+//       onLeaveBack: () => $(this).removeClass('active'),
+//     });
+// });
+
+// const data_trajectory = [
+// 	{
+// 		trId: 'trajectory1',
+// 		contId: 'trajectory-container',
+// 		trAbsId: 'absolute_trajectory1',
+// 		contAbsId: 'trajectory1',
+
+// 		start: "start 80%",
+//         end: "bottom 20%"
+// 	},
+// 	{
+// 		trId: 'trajectory2',
+// 		contId: 'trajectory1',
+// 		trAbsId: 'absolute_trajectory2',
+// 		contAbsId: 'absolute_trajectory1',
+
+// 		start: "start 80%",
+//         end: "bottom 20%"
+// 	},
+// 	{
+// 		trId: 'trajectory3',
+// 		contId: 'trajectory2',
+// 		trAbsId: 'absolute_trajectory3',
+// 		contAbsId: 'absolute_trajectory2',
+
+// 		start: "start 80%",
+//         end: "bottom 20%"
+// 	},
+// 	{
+// 		trId: 'trajectory4',
+// 		contId: 'trajectory3',
+// 		trAbsId: 'absolute_trajectory4',
+// 		contAbsId: 'absolute_trajectory3',
+
+// 		start: "start 80%",
+//         end: "bottom 20%"
+// 	},
+// 	{
+// 		trId: 'trajectory5',
+// 		contId: 'trajectory4',
+// 		trAbsId: 'absolute_trajectory5',
+// 		contAbsId: 'absolute_trajectory4',
+
+// 		start: "start 80%",
+//         end: "bottom 20%"
+// 	}
+// ]
+// for (var i = 0; i < data_trajectory.length; i++) {
+// 	console.log(data_trajectory[i]);
+
+// 	gsap.to(('#' + data_trajectory[i].trId), {
+// 	  scrollTrigger: {
+// 	      trigger: ('#' + data_trajectory[i].contId),
+// 	        scrub: true,
+// 	        markers: true,
+// 	        start: data_trajectory[i].start,
+// 	        end: data_trajectory[i].end
+// 	  },
+// 	  height: () => {
+// 	      return (120 + 'px');
+// 	  }
+// 	});
+
+// 	gsap.to(('#' + data_trajectory[i].trAbsId), {
+// 	  scrollTrigger: {
+// 	      trigger: ('#' + data_trajectory[i].contAbsId),
+// 	        scrub: true,
+// 	        markers: true,
+// 	        start: data_trajectory[i].start,
+// 	        end: data_trajectory[i].end
+// 	  },
+// 	  height: () => {
+// 	      return (120 + 'px');
+// 	  }
+// 	});
+// }
